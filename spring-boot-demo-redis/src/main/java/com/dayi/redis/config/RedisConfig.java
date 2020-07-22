eb97e0f672867b38c104f1aba1f0d2ab26572882
yyykf <1092882580@qq.com>
2020-07-22 09:54:38 +0800

    fix: 修改配置文件

--- spring-boot-demo-redis/src/main/java/com/dayi/redis/config/RedisConfig.java
+++ spring-boot-demo-redis/src/main/java/com/dayi/redis/config/RedisConfig.java
@@ -1,13 +1,12 @@
 package com.dayi.redis.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Redis配置类
@@ -18,47 +17,26 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class RedisConfig {
 
-    /**
-     * 配置Redis连接工厂
-     * 
-     * @return
-     */
-    @Bean(name = "redisConnectionFactory")
-    public RedisConnectionFactory redisConnectionFactory() {
-        // 配置连接池
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        // 设置最大空闲数、最大连接数、最大等待毫秒值
-        poolConfig.setMaxIdle(50);
-        poolConfig.setMaxTotal(100);
-        poolConfig.setMaxWaitMillis(2000L);
-        // Jedis连接工厂
-        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(poolConfig);
-        // 配置Redis连接服务器
-        RedisStandaloneConfiguration rsc = connectionFactory.getStandaloneConfiguration();
-        rsc.setHostName("127.0.0.1");
-        rsc.setPort(6379);
-
-        return connectionFactory;
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisConfig.class);
 
     /**
      * 配置RedisTemplate
      * 
      * @param connectionFactory
-     *            Redis连接工厂
+     *            Redis连接工厂，SpringBoot自动装配
      * @return
      */
     @Bean(name = "redisTemplate")
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        LOGGER.info(">>>>>>>>>>>RedisTemplate注入成功");
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
+
         // RedisTemplate会自动初始化StringRedisSerializer
         RedisSerializer<String> stringSerializer = redisTemplate.getStringSerializer();
-        // 设置String、Hash的键值对序列化方式
+        // 设置String、Hash的键序列化方式
         redisTemplate.setKeySerializer(stringSerializer);
-        redisTemplate.setValueSerializer(stringSerializer);
         redisTemplate.setHashKeySerializer(stringSerializer);
-        redisTemplate.setHashValueSerializer(stringSerializer);
 
         return redisTemplate;
     }
