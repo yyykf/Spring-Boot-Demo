3c1e91ad648a4b85746863e066081e10990d3e85
yyykf <1092882580@qq.com>
2020-07-23 11:15:34 +0800

    feat: Redis操作缓存的基础环境搭建
    1. 配置缓存管理器
    2. 整合Mybatis

--- spring-boot-demo-redis/src/main/java/com/dayi/redis/SpringBootDemoRedisApplication.java
+++ spring-boot-demo-redis/src/main/java/com/dayi/redis/SpringBootDemoRedisApplication.java
@@ -1,7 +1,9 @@
 package com.dayi.redis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 主启动类
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/7/20 17:29
  */
 @SpringBootApplication
+@MapperScan(basePackages = {"com.dayi.redis.dao"})
+@EnableCaching
 public class SpringBootDemoRedisApplication {
 
     public static void main(String[] args) {
